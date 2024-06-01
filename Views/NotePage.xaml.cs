2e0335de1dbfd4557a7b83510ef056027dc2e050
Adam Kopiec <adam.kopiec@solidcompany.pl>
2024-06-02 01:30:19 +0200

    Task 2

--- Views/NotePage.xaml.cs
+++ Views/NotePage.xaml.cs
@@ -1,56 +1,9 @@
 namespace Notes.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    public string ItemId
-    {
-        set => LoadNote(value);
-    }
-
     public NotePage()
     {
         InitializeComponent();
-
-        var appDataPath = FileSystem.AppDataDirectory;
-        var randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
-    }
-
-    private void LoadNote(string fileName)
-    {
-        var noteModel = new Models.Note
-        {
-            Filename = fileName
-        };
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
-    }
-
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-            await File.WriteAllTextAsync(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-        {
-            // Delete the file.
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-
-        await Shell.Current.GoToAsync("..");
     }
 }
